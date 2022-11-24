075f357ec77ce8ca01407b424d93dcfb8c3e0ce0
Rabih-nader <rabih.nadir@gmail.com>
2022-11-23 19:54:49 -0500

    ThoughtDetail screen finished

--- app/src/main/java/com/example/thoughts/ThoughtDetail.kt
+++ app/src/main/java/com/example/thoughts/ThoughtDetail.kt
@@ -1,8 +1,11 @@
 package com.example.thoughts
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import android.widget.TextView
 
 class ThoughtDetail : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +14,15 @@ class ThoughtDetail : AppCompatActivity() {
 
         val thoughtTitle = intent.getStringExtra("title")
         val thoughtDetail = intent.getStringExtra("content")
+        val subtitle = findViewById<TextView>(R.id.thoughtTitle)
+        val textContent = findViewById<TextView>(R.id.thoughtContent)
+        val backBtn = findViewById<ImageView>(R.id.imageView)
+
+        subtitle.text = thoughtTitle
+        textContent.text = thoughtDetail
+        backBtn.setOnClickListener {
+            val intent2 = Intent(this, MainActivity::class.java)
+            startActivity(intent2)
+        }
     }
 }
